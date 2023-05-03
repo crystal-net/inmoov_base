b08a7edabe6115557dcaeb8ef0f8d39e809975e4
crystal-net <gckemper@gmail.com>
2023-05-02 20:46:22 -0700

    upload changes

--- docs/commands.md
+++ docs/commands.md
@@ -24,11 +24,22 @@ rviz2 -d inmoov_base/config/rviz.config.rviz
 # Launch the robot_state_publisher / gazebo sim with a predefined world
 ros2 launch inmoov_base launch_sim.launch.py world:=./dev_ws/src/inmoov_base/worlds/cafe.world 
 
+ros2 launch inmoov_base launch_sim.launch.py world:=inmoov_base/worlds/empty.world 
+
 
 
 # Launch rViz2 with a launch file
 # https://answers.ros.org/question/374926/ros2-how-to-launch-rviz2-with-config-file/
 
+
+
+
+# Not sure what all this means yet but I don't want to keep typing it
+ros2 run controller_manager spawner diff_cont
+
+ros2 run controller_manager spawner joint_broad
+
+ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args -r /cmd_vel:=/diff_cont/cmd_vel_unstamped
 
 
 
--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -2,7 +2,6 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
-
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
@@ -39,9 +38,28 @@ def generate_launch_description():
 
 
 
+    # These pertain to our new ros2_controller setup
+    # See for more info: https://www.youtube.com/watch?v=4QKsDf1c4hc&t=854s
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"],
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+    )
+
+
+
+
     # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
+        diff_drive_spawner,
+        joint_broad_spawner,
     ])
